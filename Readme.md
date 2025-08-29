1039487d74644c7c568cb893201e01d34cc4521d
KQTV6965 <ahmed2.ibrahim@orange.com>
2025-08-30 00:33:17 +0200

    Final Commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+This project is a Go REST API example that uses OpenAPI (Swagger) to define endpoints and oapi-codegen to generate type-safe server code.
+It demonstrates complete CRUD operations (Create, Read, Update, Delete) for managing users, with automatic validation and documentation support.
+
+Key features:
+
+✅ API contract-first design using OpenAPI spec (openapi.yaml).
+
+✅ Generated Go types and handlers with oapi-codegen.
+
+✅ RESTful endpoints (GET, POST, PUT, DELETE).
+
+✅ CORS enabled for Swagger Editor / UI testing.
+
+✅ Uses Chi router for routing.
+
+✅ Easy integration with Swagger UI.
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	_ "strconv"
+
+	"github.com/go-chi/cors"
+
+	"GoApis/api"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type MyServer struct {
+	users map[int]api.User
+}
+
+// GET /users/{id}
+func (s *MyServer) GetUsersId(w http.ResponseWriter, r *http.Request, id int) {
+	user, ok := s.users[id]
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+// POST /users
+func (s *MyServer) PostUsersCreateUser(w http.ResponseWriter, r *http.Request) {
+	var user api.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	s.users[user.Id] = user
+	//w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(user)
+	if err != nil {
+		return
+	}
+}
+
+// PUT /users/{id}
+func (s *MyServer) PutUsersUpdateId(w http.ResponseWriter, r *http.Request, id int) {
+	userUpdated, ok := s.users[id]
+
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	var user api.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	s.users[userUpdated.Id] = user
+	//w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(user)
+	if err != nil {
+		return
+	}
+}
+
+// Delete /users/{id}
+func (s *MyServer) DeleteUsersDeleteId(w http.ResponseWriter, r *http.Request, id int) {
+	var user api.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	delete(s.users, user.Id)
+	//w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(user)
+	if err != nil {
+		return
+	}
+}
+
+// Get / users/GetAllUsers
+func (s *MyServer) GetUsersGetAllUsers(w http.ResponseWriter, r *http.Request) {
+	for _, user := range s.users {
+		err := json.NewEncoder(w).Encode(user)
+		if err != nil {
+			return
+		}
+	}
+
+	//w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func main() {
+	r := chi.NewRouter()
+	server := &MyServer{users: make(map[int]api.User)}
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}, // allow all origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by browsers
+	}))
+
+	// Register generated handlers
+	api.HandlerFromMux(server, r)
+
+	http.ListenAndServe(":8080", r)
+}
